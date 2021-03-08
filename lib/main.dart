8346fed75b8fc894df8070e0c641f0f508981850
deepakrattan <mca.deepakrattan@gmail.com>
2021-03-08 17:32:01 +0530

    Provider dependency used

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,45 +7,41 @@ class MyApp extends StatelessWidget {
   final String data = 'my data';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Level1(data),
+    return Provider(
+      create: (BuildContext context) => data,
+      child: MaterialApp(
+        home: Scaffold(
+          body: Level1(),
+        ),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
-  final String data;
-  Level1(this.data);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(data),
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
-  final String data;
-  Level2(this.data);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level3(data),
+      child: Level3(),
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  final String data;
-  Level3(this.data);
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        data,
+        Provider.of(context),
         style: TextStyle(fontSize: 30.0),
       ),
     );
