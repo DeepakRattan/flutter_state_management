47531b0de9b74aba3e719239b385703dba022baa
deepakrattan <mca.deepakrattan@gmail.com>
2021-03-08 18:04:52 +0530

    ChangeNotifier and ChangeNotifierProvider used

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:provider/provider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'my data';
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      create: (BuildContext context) => data,
+    return ChangeNotifierProvider<Data>(
+      create: (BuildContext context) => Data(),
       child: MaterialApp(
         home: Scaffold(
+          appBar: AppBar(
+            title: MyText(),
+          ),
           body: Level1(),
         ),
       ),
@@ -21,8 +23,11 @@ class MyApp extends StatelessWidget {
 class Level1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level2(),
+    return Column(
+      children: [
+        MyTextField(),
+        Level2(),
+      ],
     );
   }
 }
@@ -41,9 +46,29 @@ class Level3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        Provider.of(context),
+        Provider.of<Data>(context).data,
         style: TextStyle(fontSize: 30.0),
       ),
     );
   }
+}
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      Provider.of<Data>(context).data,
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
+
+class Data extends ChangeNotifier {
+  String data = 'Some data';
 }
